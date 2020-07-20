b3e3064613484088033167c0b6dd5d7625ac4956
unknown <you@example.com>
2020-07-20 15:58:24 -0500

    first commit

---
+++ components/Tablero.vue
@@ -0,0 +1,52 @@
+<template>
+
+<table>
+        <tr v-for="(n,i) in 3 " :key="i">
+        <td v-for="(n,j) in 3 " :key="j">
+        <casilla : token-actual="tokenActual" :ref="'casilla' +i +j"
+        @jugado="jugar"
+        :fila="i"
+        :col="j">
+    </casilla>
+     </td>
+     </tr>
+
+    </table>
+    
+</template>
+    <script>
+        export default {
+
+         name : "tablero",
+         components:{casilla},
+         data :()=>{
+         return{
+         tokenActual: "X",
+         matriz: [["", "", ""],
+         ["", "", ""],
+         ["", "", ""],]
+         }
+         },
+         metthods:{
+             jugar(e){
+             this.matriz[e.fila][e.col]=e.tokenJugado
+             if(this.verificar(e.tokenJugado)){
+             this.$emit("ganado",this)
+             }else{
+             this.tokenActual=e.tokenJugado== "X" ? "O":"X"
+             this.$emit("cambio de turno ", this)
+             }
+
+
+
+             }
+
+
+
+
+           
+
+         }
+        }
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    important: true,
+    theme: {
+      fontFamily: {
+        display: ['Gilroy', 'sans-serif'],
+        body: ['Graphik', 'sans-serif'],
+      },
+      extend: {
+        colors: {
+          cyan: '#9cdbff',
+        },
+        margin: {
+          '96': '24rem',
+          '128': '32rem',
+        },
+      }
+    },
+    variants: {
+      opacity: ['responsive', 'hover']
+    }
+  }
+
+  module.exports = {
+    theme: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+      }
+    }
+  }
+
+
+
+ 
